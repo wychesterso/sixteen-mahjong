f3b7e73080b657716c8c4a8b881c8da3440a4ee6
Chester So <chesterwyso@gmail.com>
2025-03-16 16:13:29 +1000

    structure changes

--- src/launch.bat
+++
@@ -1,4 +0,0 @@
-javac -d out/production/path game/*.java
-jar cfm SixteenMahjong.jar manifest.txt -C out/production/path .
-
-start /max cmd /k "color A2&chcp 65001&cls&title Sixteen Tiles Mahjong&java -jar SixteenMahjong.jar"
